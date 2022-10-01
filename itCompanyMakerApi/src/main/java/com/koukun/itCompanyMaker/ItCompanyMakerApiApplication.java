293e58a77c27c888d98bb7f7810b074897b3d399
koukun1996 <yasuidannsuhoru@yahoo.co.jp>
2022-10-01 22:51:30 +0900

    Spring boot 初期状態

---
+++ itCompanyMakerApi/src/main/java/com/koukun/itCompanyMaker/ItCompanyMakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.koukun.itCompanyMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItCompanyMakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItCompanyMakerApiApplication.class, args);
+	}
+
+}
---
+++ itCompanyMakerApi/src/test/java/com/koukun/itCompanyMaker/ItCompanyMakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koukun.itCompanyMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItCompanyMakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
